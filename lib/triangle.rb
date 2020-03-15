ebdcea749a4455296a5430d3e8c41aa44524349b
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-15 22:03:27 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,26 +1,26 @@
 # require 'set'
-# 
+#
 # class Triangle
-# 
+#
 #   attr_accessor :sides
-# 
+#
 #   def initialize(side1, side2, side3)
 #     @sides = [side1, side2, side3]
 #     @kind = set_kind
 #   end
-# 
+#
 #   def check
 #     valid?
 #   end
-# 
+#
 #   def kind
 #     raise TriangleError.new if !valid?
-# 
+#
 #     @kind
 #   end
-# 
+#
 #   # private
-# 
+#
 #   def valid?
 #     return false if @sides.any?{|side| side <= 0}
 #     case @kind
@@ -37,7 +37,7 @@
 #       puts "This should never be run"
 #     end
 #   end
-# 
+#
 #   def set_kind
 #     case unique_sides
 #     when 1
@@ -49,47 +49,47 @@
 #     else
 #       raise TraingleError
 #     end
-# 
+#
 #   end
-# 
+#
 #   def longest
 #     largest = 0
 #     @sides.each{|side| largest = side if side > largest}
 #     largest
 #   end
-# 
+#
 #   def smallest
 #     smallest = Float::INFINITY
 #     @sides.each{|side| smallest = side if side < smallest}
 #     smallest
 #   end
-# 
+#
 #   def unique_sides
 #     Set.new(@sides).size
 #   end
-# 
+#
 #   class Types
-# 
+#
 #     def self.isosceles
 #       :isosceles
 #     end
-# 
+#
 #     def self.scalene
 #       :scalene
 #     end
-# 
+#
 #     def self.equilateral
 #       :equilateral
 #     end
-# 
+#
 #   end
-# 
+#
 #   class TriangleError < StandardError
 #     def message
 #       "This triangle is not valid"
 #     end
 #   end
-# 
+#
 # end
 # class Triangle
 #   # write code here\
